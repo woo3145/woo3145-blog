e532aab93d2801642995e1597401e02bf3a1b2d1
woo3145 <lcwoo3145@gmail.com>
2022-11-18 08:57:01 +0900

    Work - Utterances darkMode 추가

--- components/modules/Utterances.tsx
+++ components/modules/Utterances.tsx
@@ -1,25 +1,38 @@
-import { useEffect, useRef } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
 
 const Utterances = () => {
-  const ref = useRef<HTMLDivElement>(null);
+  const uttLight = useRef<HTMLDivElement>(null);
+  const uttDark = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const createUtterance = (ref: RefObject<any>, theme: 'dark' | 'light') => {
     const script = document.createElement('script');
-
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('crossorigin', 'anonymous');
-    script.setAttribute('async', 'true');
+    script.async = true;
     script.setAttribute(
       'repo',
       process.env.NEXT_PUBLIC_COMMENT_GITHUB_REPO || ''
     );
     script.setAttribute('issue-term', 'title');
     script.setAttribute('label', 'blog-comment');
-    script.setAttribute('theme', 'github-light');
+    script.setAttribute(
+      'theme',
+      theme === 'dark' ? 'github-dark' : 'github-light'
+    );
     ref.current?.appendChild(script);
+  };
+
+  useEffect(() => {
+    createUtterance(uttDark, 'dark');
+    createUtterance(uttLight, 'light');
   }, []);
 
-  return <div ref={ref}></div>;
+  return (
+    <div className="border-t pt-8">
+      <div ref={uttLight} className="dark:hidden"></div>
+      <div ref={uttDark} className="hidden dark:block"></div>
+    </div>
+  );
 };
 
 export default Utterances;
